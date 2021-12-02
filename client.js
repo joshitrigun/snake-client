b91162b068f14e140d9e0822c702f421be9bff61
Trigun Joshi <trigunjoshi@gmail.com>
2021-12-01 18:13:34 -0800

    client added

--- client.js
+++ client.js
@@ -1,5 +1,6 @@
 const net = require("net");
-const connect = function () {
+
+const connect = () => {
   const conn = net.createConnection({
     host: "165.227.47.243", // IP address here,
     port: 50541, // PORT number here,
@@ -7,11 +8,25 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    conn.write("Name: TJ");
-  });
+
   conn.on("data", (data) => {
     console.log(data);
+  });
+  //const arrayDir = ["Move: up", "Move: right", "Move: up"];
+  let startingTime = 50;
+
+  conn.on("connect", () => {
+    conn.write(`Name: XXX`);
+    // for (let arr of arrayDir) {
+    //   setTimeout(() => {
+    //     conn.write(arr);
+    //   }, startingTime);
+    //   startingTime += 300;
+    // }
+
+    // setInterval(() => {
+    //   conn.write("Move: up");
+    // }, startingTime);
   });
   return conn;
 };
--- play.js
+++ play.js
@@ -3,6 +3,22 @@ const { connect } = require("./client");
 // establishes a connection with the game server
 
 console.log("Connecting ...");
-connect();
+//connect();
+const con = connect();
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+const handleUserInput = function (data) {
+  // your code here
+  if (data === "\u001b[A") con.write("Move: up");
+  if (data === "\u001b[B") con.write("Move: down");
+  if (data === "\u001b[C") con.write("Move: right");
+  if (data === "\u001b[D") con.write("Move: left");
+};
 
-
+setupInput();
